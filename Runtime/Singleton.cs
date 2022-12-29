8ed01bf5cfe113a04345289f5309f2f18ba042ad
david444456 <david-toranzo-@hotmail.com>
2022-12-29 14:56:02 -0300

    [A] add singleton scripts, modified assemblies

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Patterns.Singleton
+{
+    public abstract class Singleton<TypeSingleton> : MonoBehaviour where TypeSingleton : MonoBehaviour
+    {
+        protected static TypeSingleton _instance;
+
+        public static TypeSingleton Instance
+        {
+            get
+            {
+                if (null == _instance)
+                    throw new System.Exception("There isn't a instance of :" + typeof(TypeSingleton) +" in the scene");
+
+                return _instance;
+            }
+        }
+
+        protected void Awake()
+        {
+            InitializeSingleton();
+            AwakeAfterInitializeSingleton();
+        }
+
+        protected void InitializeSingleton()
+        {
+            if (null == _instance)
+                _instance = gameObject.GetComponent<TypeSingleton>();
+            else if (_instance != this)
+                Destroy(gameObject);
+        }
+
+        protected abstract void AwakeAfterInitializeSingleton();
+
+        protected void DontDestroyWhenLoadScene()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
